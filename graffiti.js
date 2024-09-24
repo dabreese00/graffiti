c046faf07e0d564306fad137c6b1a4910c2976fd
David Breese <dabreese00@gmail.com>
2024-09-24 00:02:20 -0500

    Make loading more robust and add some tags

--- graffiti.js
+++ graffiti.js
@@ -2,28 +2,42 @@ const url = browser.runtime.getURL("taggers/my_taggers.json");
 
 const myRequest = new Request(url);
 
-fetch(myRequest)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error, status = ${response.status}`);
-    }
-    return response.text();
-  })
+function tagTheDOM() {
+  fetch(myRequest)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error, status = ${response.status}`);
+      }
+      return response.text();
+    })
+  
+    .then((myText) => {
+      const my_taggers = JSON.parse(myText);
+  
+      for (const tagger of my_taggers) {
+        matcher = RegExp(tagger.matcher);
+  
+        if (matcher.test(window.location)) {
+          for (const tag of tagger.tags) {
+            elements = document.querySelectorAll(tag.selector)
 
-  .then((myText) => {
-    const my_taggers = JSON.parse(myText);
-
-    for (const tagger of my_taggers) {
-      matcher = RegExp(tagger.matcher);
-
-      if (matcher.test(window.location)) {
-        for (const tag of tagger.tags) {
-          document.querySelector(tag.selector).style.setProperty(tag.property, tag.value);
+            for (const e of elements) {
+              console.debug(`Applying styles to ${e.tagName}`);
+              e.style.setProperty(tag.property, tag.value);
+            }
+          }
         }
       }
-    }
-  })
+    })
+  
+    .catch((error) => {
+      console.error(`Error: ${error.name}: ${error.message}`);
+    });
+};
 
-  .catch((error) => {
-    console.error(`Error: ${error.name}: ${error.message}`);
-  });
+if (document.readyState === "loading") {
+  // Loading hasn't finished yet
+  document.addEventListener("DOMContentLoaded", tagTheDOM);
+} else {
+  tagTheDOM();
+}
